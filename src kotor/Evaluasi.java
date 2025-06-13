9f15c9150d13b1900750011402ef4821894cf6e5
Assyiekril <arilsarda21@gmail.com>
2025-06-14 00:41:33 +0800

    chore: hapus folder 'src kotor' karena tidak diperlukan

--- src kotor/Evaluasi.java
+++
@@ -1,7 +0,0 @@
-package model;
-interface Evaluasi {
-    public void evalBB(double bbBaru);
-    public void evalBulking(double perubahanBB);
-    public void evalCutting(double perubahanBB);
-    public void evalStamina(double perubahanBB);
-}
